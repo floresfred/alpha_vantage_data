7dd49511ac7edbe0cd27c8dc9e83ceee9013f3b1
Jacob Renn <jwrenn4@outlook.com>
2019-06-21 19:04:01 -0400

    adding package functionality

--- AlphaVantage/__init__.py
+++ AlphaVantage/__init__.py
@@ -1,1 +1,4 @@
-#TODO: create the init file
+__version__ = '0.0.1'
+__dev__ = True
+
+from AlphaVantage import AlphaVantage
--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-#TODO: create this setup file
+from distutils.core import setup
+from AlphaVantage.__init__ import __version__
+
+setup(
+name = 'AlphaVantage',
+version = __version__,
+packages = ['AlphaVantage'],
+author_name = 'Jacob Renn',
+author_email = 'jwrenn4@outlook.com',
+install_requires = ['requests'],
+)
